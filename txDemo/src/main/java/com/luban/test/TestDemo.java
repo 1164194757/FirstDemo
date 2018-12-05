84767df2244c2d2230cf0c38329cf5592c6a67d5
Robert <18768379183@163.com>
2018-12-05 19:40:32 +0800

    诶

--- txDemo/src/main/java/com/luban/test/TestDemo.java
+++ txDemo/src/main/java/com/luban/test/TestDemo.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.luban.service.BuyService;
 
 public class TestDemo {
-    public static void main(String[] args) {
+    @SuppressWarnings("resource")
+	public static void main(String[] args) {
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(App.class);
         BuyService buyService = (BuyService) annotationConfigApplicationContext.getBean("buyService");
         buyService.update();
